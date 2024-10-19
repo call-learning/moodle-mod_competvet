2f12c42f44bf4e5b15018e75792f5127750640ed
Laurent David <lmedavid@gmail.com>
2024-10-19 09:42:37 +0200

    Update version to 1.8.0

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_competvet';
-$plugin->release = '1.5.0';
+$plugin->release = '1.8.0';
 $plugin->version = 2024091705;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
