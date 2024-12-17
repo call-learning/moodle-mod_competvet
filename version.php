6e5005ec526ea09974cf82e936aed4b87215dcc4
Laurent David <lmedavid@gmail.com>
2024-12-17 09:00:56 +0100

    Update to 2.2.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_competvet';
-$plugin->release = '2.2.0';
-$plugin->version = 2024120701;
+$plugin->release = '2.2.1';
+$plugin->version = 2024121700;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
