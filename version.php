e3870debb3d7b68da971a44c21a4488d583521be
Laurent David <lmedavid@gmail.com>
2025-08-18 18:23:19 +0200

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_competvet';
-$plugin->release = '2.5.4';
-$plugin->version = 2025080201;
+$plugin->release = '2.5.5';
+$plugin->version = 2025081801;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_ALPHA;
